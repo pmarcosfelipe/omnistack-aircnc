ffd2896f8ff9af3917008195ef506e61b2255d72
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-11-19 14:18:56 -0300

    Refactor backend to create new local file to get thumbnail image and did dashboard screen with spot list

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 
@@ -19,6 +20,7 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
